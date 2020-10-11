ee1314d7491f615082985869175b63d0f46646fd
Olga <mironova_plus@mail.ru>
2020-10-11 14:30:52 +0300

    Добавляет дополнительные функции для создания массива с данными

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@
 const DATA_ARRAY_LENGTH = 8;
 const DATA_ARRAY = [];
 const MAX_PRICE_NIGHT = 1000000;
-const MIN_HOUSE_PRICES = [10000, 1000, 5000, 0];
-const HOUSE_TYPE = [`Дворец`, `Квартира`, `Дом`, `Бунгало`];
+const HOUSE_TYPE = [`palace`, `flat`, `house`, `bungalow`];
 const CHECKIN_CHECKOUT = [`12:00`, `13:00`, `14:00`];
 const ROOM_NUMBERS = [100, 2, 3, 1];
 const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
@@ -19,40 +18,64 @@ const PIN_SHIFT_Y = 22;
 let pinTemplate = document.querySelector(`#pin`).content;
 
 // Functions
-// Функция генерации случайных данных
+// Функция генерации случайных данных от min до max
 let getRandomNumber = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+// возвращает int on 0 до max
+let getRandomInt = function (max) {
+  return Math.floor(Math.random() * Math.floor(max));
+};
 
 // Функция заполнения DOM-элементами на основе массива JS-объектов
+let getObjectDataArray = function (index) {
+  const card = {
+    author: {
+      avatar: `img/avatars/user0${index + 1}.png`
+    },
+    offer: {
+      title: `Уютное гнездо №${index + 1}`,
+      address: `${getRandomNumber(200, 700)}, ${getRandomNumber(150, 450)}`,
+      price: getRandomInt(MAX_PRICE_NIGHT),
+      rooms: ROOM_NUMBERS[getRandomInt(ROOM_NUMBERS.length - 1)],
+      guests: GUESTS_NUMBER[getRandomInt(GUESTS_NUMBER.length - 1)],
+      checkin: CHECKIN_CHECKOUT[getRandomInt(CHECKIN_CHECKOUT.length - 1)],
+      checkout: CHECKIN_CHECKOUT[getRandomInt(CHECKIN_CHECKOUT.length - 1)],
+      features: FEATURES.slice(0, getRandomNumber(1, FEATURES.length)),
+      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+      photos: PICTURE_SRC.slice(0, getRandomNumber(1, PICTURE_SRC.length))
+    },
+    location: {
+      x: getRandomNumber(0, 1000),
+      y: getRandomNumber(130, 630)
+    }
+  };
+
+  switch (HOUSE_TYPE[getRandomInt(HOUSE_TYPE.length - 1)]) {
+    case `palace`:
+      card.offer.type = `Дворец`;
+      break;
+    case `flat`:
+      card.offer.type = `Квартира`;
+      break;
+    case `house`:
+      card.offer.type = `Дом`;
+      break;
+    case `bungalow`:
+      card.offer.type = `Бунгало`;
+      break;
+  }
+
+  return card;
+};
+
 let getObjectsDataArray = function () {
   for (let i = 0; i < DATA_ARRAY_LENGTH; i++) {
-    const card = {
-      author: {
-        avatar: `img/avatars/user0${i + 1}.png`
-      },
-      offer: {
-        title: `Уютное гнездо №${i + 1}`,
-        address: `${getRandomNumber(200, 700)}, ${getRandomNumber(150, 450)}`,
-        price: getRandomNumber(MIN_HOUSE_PRICES[i % MIN_HOUSE_PRICES.length], MAX_PRICE_NIGHT),
-        type: HOUSE_TYPE[i % HOUSE_TYPE.length],
-        rooms: ROOM_NUMBERS[i % ROOM_NUMBERS.length],
-        guests: GUESTS_NUMBER[i % GUESTS_NUMBER.length],
-        checkin: CHECKIN_CHECKOUT[i % CHECKIN_CHECKOUT.length],
-        checkout: CHECKIN_CHECKOUT[i % CHECKIN_CHECKOUT.length],
-        features: FEATURES.slice(0, i % FEATURES.length + 1),
-        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-        photos: PICTURE_SRC.slice(0, i % PICTURE_SRC.length + 1)
-      },
-      location: {
-        x: getRandomNumber(0, 1000),
-        y: getRandomNumber(130, 630)
-      }
-    };
-    DATA_ARRAY[i] = card;
+    DATA_ARRAY[i] = getObjectDataArray(i);
   }
+  return DATA_ARRAY;
 };
 
 let createPinElement = function (pin) {
